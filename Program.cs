34f12cc2fcd0e7d1f6f7a5538b9c26fadaba9f2d
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-14 22:28:49 +0530

    Routing in depth is done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is my first ASP.NET Core empty web app");
+/*
+ * ASP.NET Core is crucial for setting up routing in your web application.
+ * UseRouting adds the routing middleware to the application's request pipeline. 
+ * This middleware is responsible for matching incoming HTTP requests
+ * to the endpoints defined in your application.
+ * When a request is received, the routing middleware checks 
+ * the request's path and method against the route templates defined in your app. 
+ * Once a matching endpoint is found, the routing middleware prepares the endpoint 
+ * to be executed later in the pipeline.
+ * 
+ */
+app.UseRouting();
 
+app.UseEndpoints(endpoint =>
+{
+    //Define routes
+
+    endpoint.Map("/Home", async (context) =>
+    {
+        await context.Response.WriteAsync($"You are in Home Page.");
+    });
+
+    endpoint.MapGet("/Product", async (context) =>
+    {
+        await context.Response.WriteAsync($"You are in Products Page.");
+    });
+
+    endpoint.MapPost("/Contact", async (context) =>
+    {
+        await context.Response.WriteAsync($"You are in Contact Page.");
+    });
+});
+
+app.Run(async (HttpContext context) => { 
+    await context.Response.WriteAsync(
+        $"The page you are looking for is not found.");
+});
 app.Run();
+
+
