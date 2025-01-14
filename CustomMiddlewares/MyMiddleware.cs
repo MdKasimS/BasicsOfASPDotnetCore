990b6c935935e1496c5cd19c9fffd4a86b163219
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-14 17:37:54 +0530

    Added custom middleware

--- CustomMiddlewares/MyMiddleware.cs
+++ CustomMiddlewares/MyMiddleware.cs
@@ -1,6 +1,19 @@
-﻿namespace CalculatorASPWebApp.CustomMiddlewares
+﻿
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace CalculatorASPWebApp.CustomMiddlewares
 {
-    public class MyMiddleware
-{
-}
+    public class MyMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            //Before Logic
+            await context.Response.WriteAsync("Begin: Custom Middleware Started....\n");
+            await next(context);
+
+            //After Logic
+            await context.Response.WriteAsync("End  : Custom Middleware Ended....\n");
+
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using CalculatorASPWebApp.CustomMiddlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//provides instance using DI
+builder.Services.AddTransient<MyMiddleware>();
 
 //Creates an instance of our web application.
 var app = builder.Build();
@@ -27,7 +32,11 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
     await context.Response.WriteAsync("End  : Middleware_2\n");
 });
 
-//Middleware 3
+//Middleware 3 - custom middleware
+app.UseMiddleware<MyMiddleware>();
+
+
+//Middleware 4
 //Its a Treminal middleware aks ShortCircuiting middleware
 app.Run(async (HttpContext context) =>
 {
