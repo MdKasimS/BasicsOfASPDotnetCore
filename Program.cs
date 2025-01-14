a81702dde2ada55f27ac649165f84a94e27bd89b
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-14 14:12:42 +0530

    Query string

--- Program.cs
+++ Program.cs
@@ -25,23 +25,62 @@ var app = builder.Build();
  *  
  *  This can also receive HttpContext object parameter.
  */
-app.MapGet("/", (HttpContext context) =>
-{
-    /*string path = context.Request.Path;
-     * string method = context.Request.Method;
-     * int status = context.Response.StatusCode;
-     */
-    /*var UserAgent = "";
-     *if (context.Request.Headers.ContainsKey("User-Agent"))
-     *{
-     *    //All keys are going to be string in headers
-     *    UserAgent = context.Request.Headers["User-Agent"];
-     *    context.Response.StatusCode = 200;
-     */
+//app.MapGet("/", (HttpContext context) =>
+//{
+//    /*string path = context.Request.Path;
+//     * string method = context.Request.Method;
+//     * int status = context.Response.StatusCode;
+//     */
+//    /*var UserAgent = "";
+//     *if (context.Request.Headers.ContainsKey("User-Agent"))
+//     *{
+//     *    //All keys are going to be string in headers
+//     *    UserAgent = context.Request.Headers["User-Agent"];
+//     *    context.Response.StatusCode = 200;
+//     */
 
-    context.Response.Headers["Content-Type"] = "text/html";
-    context.Response.Headers["MyHeader"] = "Hello World!";
-    return $"<h2>This is text response</h2>";
+//    context.Response.Headers["Content-Type"] = "text/html";
+//    context.Response.Headers["MyHeader"] = "Hello World!";
+//    return $"<h2>This is text response</h2>";
+//});
+
+app.Run(async (HttpContext context)=>
+{
+    string path = context.Request.Path.Value ?? string.Empty;
+
+    //check case sensitivity for Home and home
+    //not handling /home/ or say /product/
+    if (path=="/" || path=="/Home" || path == "/home")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("You are in Home Page.");
+    }
+    else if(path=="/Contact" || path == "/contact")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("You are in Contact Page.");
+    }
+    else if (path == "/Product" || path == "/product")
+    {
+        //You have to set the response before sending the body
+        context.Response.StatusCode = 200;
+
+        if(context.Request.Query.ContainsKey("id") &&
+            context.Request.Query.ContainsKey("name"))
+        {
+            string id = context.Request.Query["id"];
+            string name = context.Request.Query["name"];
+            await context.Response.WriteAsync($"You selected with product id:{id} and {name}");
+
+            return;
+        }
+        await context.Response.WriteAsync("You are in Product Page.");
+    }
+    else
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("The page you are looking for not found.");
+    }
 });
 
 app.Run();
