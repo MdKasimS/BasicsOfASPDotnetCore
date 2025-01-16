0820a64c72a3e6e362285565e050ec25e1270650
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 16:12:53 +0530

    Multiple route parameters is done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+//Creates instance of our Web Application
 var app = builder.Build();
 
 app.UseRouting();
-
 app.UseEndpoints(endpoint =>
 {
     endpoint.MapGet("/products/{id}", async (context) =>
     {
         var id = Convert.ToInt32(context.Request.RouteValues["id"]);
-        await context.Response.WriteAsJsonAsync($"This is product id = {id}");
+        await context.Response.WriteAsync($"This is product id = {id}");
+    });
+
+    endpoint.MapGet("/books/{bookId}/{authorName}", async (context) =>
+    {
+        var BookId = Convert.ToInt32(context.Request.RouteValues["bookId"]);
+        var Author = context.Request.RouteValues["authorName"].ToString();
+        await context.Response.WriteAsync($"Book Details\n Id: {BookId} \n Author Name :{Author}\n");
     });
 });
 
@@ -16,7 +23,5 @@ app.Run(async (HttpContext context) =>
 {
     await context.Response.WriteAsync("Welcome to my ASP.NETCore app.");
 });
-
-
 
 app.Run();
