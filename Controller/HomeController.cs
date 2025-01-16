5aa51095802c2d86bfe55cd7e1db710b9d572681
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 23:08:51 +0530

    Additional routes, problem with app.Run(asycn(HtppContext context)=>{}); for outher routes whne used

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,6 +1,39 @@
-﻿namespace CalculatorASPWebApp.Controller
+﻿//using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorASPWebApp.Controller
 {
+    
     public class HomeController
-{
+    {
+        [Route("Home")]
+        public string Index()
+        {
+            return "Welcome To Home Controller";
+        }
+
+        [Route("About")]
+        public string About()
+        {
+            return "You are in About Page.";
+        }
+
+        [Route("Contact-us")]
+        public string Contact()
+        {
+            return "You are in Contact Us Page.";
+        }
+
+        [Route("/products/{id:int:min(1000):max(9999)}")]
+        public string Products()
+        {
+            return "You are in Products Page.";
+        }
+
+        
+
+    }
 }
-}
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 using CalculatorASPWebApp.Controller;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddTransient<HomeController>();
 
+//builder.Services.AddTransient<HomeController>();
 builder.Services.AddControllers();
 
+//Creates instance of our Web Application
 var app = builder.Build();
 
+app.MapControllers();
+
+//app.UseRouting();
+//app.UseEndpoints(endpoint =>
+//{
+//    endpoint.MapControllers();
+
+//});
 app.MapGet("/", () => "This is my first ASP.NET Core empty web app");
+
+/*This line when un commented for attribute basd routing using controllers
+ * doesn't allow other routes to match
+ */
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("The page you are looking not found.");
+//});
+
 
 app.Run();
