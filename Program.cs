499d2c145e9fb12d86e815dcc726401a635c4a12
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 22:25:17 +0530

    Adding controllers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using CalculatorASPWebApp.Controller;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddTransient<HomeController>();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "This is my first ASP.NET Core empty web app");
