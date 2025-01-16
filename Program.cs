632ace1918db6ed272bb9374e59378649c794dba
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 14:07:05 +0530

    First route parameter handling- basic case

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is my first ASP.NET Core empty web app");
+app.UseRouting();
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapGet("/products/{id}", async (context) =>
+    {
+        var id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsJsonAsync($"This is product id = {id}");
+    });
+});
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Welcome to my ASP.NETCore app.");
+});
+
+
 
 app.Run();
