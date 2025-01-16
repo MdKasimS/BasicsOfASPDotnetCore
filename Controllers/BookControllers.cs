f7071556a047b8b9317531ae38ad5e963b8cdc4a
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-17 01:05:18 +0530

    Redirecting request from one controller to another

--- Controllers/BookControllers.cs
+++ Controllers/BookControllers.cs
@@ -8,23 +8,27 @@ namespace CalculatorASPWebApp.Controllers
         [Route("/Books")]
         public IActionResult Book()
         {
-            if (!Request.Query.ContainsKey("BookId"))
-            {
-                //Response.StatusCode = 500;
-                return BadRequest("Book Id is Not provided!");
-            }
-            int bookId = Convert.ToInt32(Request.Query["BookId"]);
-            if (bookId < 1 || bookId > 1000)
-            {
-                return NotFound("Book ID is not in range of 1 too 1000!");
-            }
-
-            if (!Convert.ToBoolean(Request.Query["IsLogged"]))
-            {
-                return Unauthorized("You are not logged in!");
-            }
-
-            return File("/Samples/Sample.pdf", "application/pdf");
+            return RedirectToAction("Books", "Store", new { });
         }
     }
 }
+
+
+
+//if (!Request.Query.ContainsKey("BookId"))
+//{
+//    //Response.StatusCode = 500;
+//    return BadRequest("Book Id is Not provided!");
+//}
+//int bookId = Convert.ToInt32(Request.Query["BookId"]);
+//if (bookId < 1 || bookId > 1000)
+//{
+//    return NotFound("Book ID is not in range of 1 too 1000!");
+//}
+
+//if (!Convert.ToBoolean(Request.Query["IsLogged"]))
+//{
+//    return Unauthorized("You are not logged in!");
+//}
+
+//return File("/Samples/Sample.pdf", "application/pdf");
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorASPWebApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("/category/books")]
+        public IActionResult Books()
+        {
+            return Content("New Book Page", "text/plain");
+        }
+    }
+}
