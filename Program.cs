4507d7f5fa767f06171109a2667a506dee7bd0b5
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 17:34:27 +0530

    Got DI conept clarity with context object

--- Program.cs
+++ Program.cs
@@ -5,42 +5,41 @@ var app = builder.Build();
 app.UseRouting();
 app.UseEndpoints(endpoint =>
 {
-    endpoint.MapGet("/products/id/{id=100}", async (context) =>
+    endpoint.MapGet("/products/id/{id:int}", async (context1) =>
     {
-        var id = context.Request.RouteValues["id"];
+        var id = context1.Request.RouteValues["id"];
 
         if (id != null)
         {
             id = Convert.ToInt32(id);
-            await context.Response.WriteAsync($"This is product id = {id}");
+            await context1.Response.WriteAsync($"This is product id = {id}");
 
         }
         else
         {
-            await context.Response.WriteAsync($"You are in Products Page.");
+            await context1.Response.WriteAsync($"You are in Products Page.");
         }
     });
 
-    endpoint.MapGet("/books/{authorName='kasim'}/{bookId?}", async (context) =>
+    endpoint.MapGet("/books/{authorName='kasim'}/{bookId?}", async (context2) =>
     {
-        var Author = context.Request.RouteValues["authorName"].ToString();
-        var BookId = context.Request.RouteValues["bookId"];
+        var Author = context2.Request.RouteValues["authorName"].ToString();
+        var BookId = context2.Request.RouteValues["bookId"];
         if (BookId != null)
         {
             BookId = Convert.ToInt32(BookId);
-            await context.Response.WriteAsync($"Book Details\n Id: {BookId} \n Author Name :{Author}\n");
-
+            await context2.Response.WriteAsync($"Book Details\n Id: {BookId} \n Author Name :{Author}\n");
         }
         else
         {
-            await context.Response.WriteAsync($"Displaying All Books By {Author}:-");
+            await context2.Response.WriteAsync($"Displaying All Books By {Author}:-");
         }
     });
 });
 
-app.Run(async (HttpContext context) =>
+app.Run(async (HttpContext context3) =>
 {
-    await context.Response.WriteAsync("Welcome to my ASP.NETCore app.");
+    await context3.Response.WriteAsync("Welcome to my ASP.NETCore app.");
 });
 
 app.Run();
