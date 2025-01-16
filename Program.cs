0e21ac43518755273ee5b29b89cc5207ff4a885a
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-16 19:37:12 +0530

    Route constraint - regex

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
-//Creates instance of our Web Application
 var app = builder.Build();
-
 app.UseRouting();
 app.UseEndpoints(endpoint =>
 {
-    endpoint.MapGet("/products/id/{id:int}", async (context1) =>
+    endpoint.MapGet("/books/{id:int}", async (context1) =>
     {
         var id = context1.Request.RouteValues["id"];
 
@@ -21,7 +19,7 @@ app.UseEndpoints(endpoint =>
         }
     });
 
-    endpoint.MapGet("/books/{authorName:alpha:minlength(5)}/{bookId:int}", async (context2) =>
+    endpoint.MapGet("/books/{authorName:alpha:length(5,15)}/{bookId:int}", async (context2) =>
     {
         var Author = context2.Request.RouteValues["authorName"].ToString();
         var BookId = context2.Request.RouteValues["bookId"];
@@ -35,11 +33,23 @@ app.UseEndpoints(endpoint =>
             await context2.Response.WriteAsync($"Displaying All Books By {Author}:-");
         }
     });
-});
 
+    endpoint.MapGet("/quarterly-results/{year:int:min(1999)}/{month:regex(^(mar|jun|sep|dec)$)}", async (context1) =>
+    {
+        int year = Convert.ToInt32(context1.Request.RouteValues["year"]);
+        string month = (context1.Request.RouteValues["month"].ToString());
+        await context1.Response.WriteAsync($"Quarteyly Resulst For {year}-{month}");
+    });
+
+    endpoint.MapGet("/daily-results/{date:regex(^(19|21)$)}", async (context1) =>
+    {
+        int year = Convert.ToInt32(context1.Request.RouteValues["year"]);
+        string month = (context1.Request.RouteValues["month"].ToString());
+        await context1.Response.WriteAsync($"Quarteyly Resulst For {year}-{month}");
+    });
+});
 app.Run(async (HttpContext context3) =>
 {
-    await context3.Response.WriteAsync("Welcome to my ASP.NETCore app.");
+    await context3.Response.WriteAsync($"Welcome to my ASP.NETCore app.{context3.Request.Path}");
 });
-
 app.Run();
